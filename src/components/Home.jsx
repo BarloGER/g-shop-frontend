31482532c1b739caa696f0bfca42c70926ef946f
BarloGER <barlozewski.mike@gmail.com>
2023-01-22 10:11:32 +0100

    added top row to Home.js

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,38 @@
+import { FaSearch, FaShoppingBasket, FaAngleDown } from "react-icons/fa";
+import "../styles/home.css";
+
+// ToDo: Install Browser Router and change a to LINK
+
 export default function Home() {
   return (
     <>
-      <h1>Willkommen</h1>
+      <header>
+        <div className="top">
+          <a href="/" className="icon search-icon">
+            <FaSearch size="2.5rem" />
+          </a>
+
+          <ul>
+            <li>
+              <a href="/">De</a>
+            </li>
+            <li>
+              <a href="/">En</a>
+            </li>
+            <li>
+              <a href="/">
+                Konto
+                <FaAngleDown className="arrow-icon" />
+              </a>
+            </li>
+            <li>
+              <a href="/" className="icon basket-icon" data-count="0">
+                <FaShoppingBasket size="2rem" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
     </>
   );
 }
