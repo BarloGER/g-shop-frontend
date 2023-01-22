bed097e4b906402ade33d34eacf6dca491c40545
BarloGER <barlozewski.mike@gmail.com>
2023-01-22 12:27:35 +0100

    added expanded acc and searchbar menu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,84 @@
-import { FaSearch, FaShoppingBasket, FaAngleDown } from "react-icons/fa";
+import { useState } from "react";
+import {
+  FaSearch,
+  FaShoppingBasket,
+  FaAngleDown,
+  FaAngleUp,
+  FaTimes,
+} from "react-icons/fa";
 import "../styles/home.css";
 
 // ToDo: Install Browser Router and change a to LINK
+// ToDo: Move Searchbar to own component
 
 export default function Home() {
+  const [isAccExpanded, setIsAccExpanded] = useState(false);
+  const [isSearchBarExpanded, setIsSearchBarExpanded] = useState(false);
+
   return (
     <>
       <header>
         <div className="top">
-          <a href="/" className="icon search-icon">
+          <a
+            href="#"
+            className={
+              !isSearchBarExpanded
+                ? "icon search-icon"
+                : "icon search-icon-expanded"
+            }
+            onClick={() => {
+              setIsSearchBarExpanded(!isSearchBarExpanded);
+            }}
+          >
             <FaSearch size="2.5rem" />
           </a>
+          <div
+            className={
+              !isSearchBarExpanded ? "search-bar" : "search-bar-expanded"
+            }
+          >
+            <form>
+              <input type="text" placeholder="Suchbegriff eingeben..." />
+            </form>
+            <a href="#">
+              <FaTimes
+                size="2.5rem"
+                className="cross-icon"
+                onClick={() => {
+                  setIsSearchBarExpanded(!isSearchBarExpanded);
+                }}
+              />
+            </a>
+          </div>
 
           <ul>
             <li>
-              <a href="/">De</a>
+              <a href="#">De</a>
             </li>
             <li>
-              <a href="/">En</a>
+              <a href="#">En</a>
             </li>
-            <li>
-              <a href="/">
+            <li className={isAccExpanded ? "acc" : "acc-expanded"}>
+              <a
+                href="#"
+                onClick={() => {
+                  setIsAccExpanded(!isAccExpanded);
+                }}
+              >
                 Konto
-                <FaAngleDown className="arrow-icon" />
+                {!isAccExpanded ? (
+                  <FaAngleDown className="arrow-icon" />
+                ) : (
+                  <FaAngleUp className="arrow-icon" />
+                )}
               </a>
+              <div className={!isAccExpanded ? "acc-div" : "acc-div-expanded"}>
+                <a href="#">Merkzettel (0)</a>
+                <button>Anmelden</button>
+              </div>
             </li>
             <li>
-              <a href="/" className="icon basket-icon" data-count="0">
+              <a href="#" className="icon basket-icon" data-count="0">
                 <FaShoppingBasket size="2rem" />
               </a>
             </li>
