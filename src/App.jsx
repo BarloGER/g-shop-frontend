be8984f07dcb2f91b208388cc75e9f60762bd954
BarloGER <barlozewski.mike@gmail.com>
2023-01-31 16:00:08 +0100

    Implemented authentication with states, routes into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -8,13 +9,81 @@ import {
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
+import ProtectedRoute from "./components/ProtectedRoute";
+import UserProfile from "./components/UserProfile";
+import NotFound from "./components/NotFound";
+import { getUser } from "./utils/auth";
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loadingAuthRequest, setLoadingAuthRequest] = useState(false);
+
+  useEffect(() => {
+    const validateToken = async () => {
+      try {
+        setLoadingAuthRequest(true);
+        const { data, error } = await getUser(token);
+        if (error) throw error;
+        setUser(data);
+        setIsAuthenticated(true);
+        setLoadingAuthRequest(false);
+      } catch (error) {
+        localStorage.removeItem("token");
+        setToken(null);
+        setLoadingAuthRequest(false);
+        console.log(error);
+      }
+    };
+    token && validateToken();
+  }, [token]);
+
+  const logOut = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    setUser(null);
+    setIsAuthenticated(false);
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
+      <Route
+        path="/"
+        element={
+          <Layout
+            isAuthenticated={isAuthenticated}
+            setIsAuthenticated={setIsAuthenticated}
+            setToken={setToken}
+            loadingAuthRequest={loadingAuthRequest}
+            setLoadingAuthRequest={setLoadingAuthRequest}
+            logOut={logOut}
+            user={user}
+          />
+        }
+      >
         <Route index element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
+
+        <Route
+          path="/signup"
+          element={
+            <SignUp
+              isAuthenticated={isAuthenticated}
+              setIsAuthenticated={setIsAuthenticated}
+              setToken={setToken}
+              loadingAuthRequest={loadingAuthRequest}
+              setLoadingAuthRequest={setLoadingAuthRequest}
+            />
+          }
+        />
+        <Route
+          path="auth"
+          element={<ProtectedRoute isAuthenticated={isAuthenticated} />}
+        >
+          <Route index element={<UserProfile user={user} />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
       </Route>
     )
   );
