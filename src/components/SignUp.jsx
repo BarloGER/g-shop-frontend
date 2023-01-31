611f0b94aa17d49db0bad4c63be3afe3d4fb3bfd
BarloGER <barlozewski.mike@gmail.com>
2023-01-31 16:01:11 +0100

    Implemented authentication into SignUp

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,99 @@
+import { useState } from "react";
+import { registerUser } from "../utils/auth";
+import { Navigate } from "react-router-dom";
+import Loading from "./Loading";
 import "../styles/signUp.css";
 
 // ToDo: Add Input Fields for business customer
 // ToDo: Add second personal-data form for alternative address
-// ? Maybe add Datenschutz hint
+// ? Maybe add "Datenschutz" hint
 
-const SignUp = () => {
+const SignUp = ({
+  isAuthenticated,
+  setToken,
+  setIsAuthenticated,
+  loadingAuthRequest,
+  setLoadingAuthRequest,
+}) => {
+  const [
+    {
+      email,
+      password,
+      salutation,
+      firstname,
+      lastname,
+      birth_date,
+      zip_code,
+      city,
+      street,
+      street_number,
+      country,
+      tel,
+    },
+    setFormState,
+  ] = useState({
+    email: "",
+    password: "",
+    salutation: "",
+    firstname: "",
+    lastname: "",
+    birth_date: "",
+    zip_code: "",
+    city: "",
+    street: "",
+    street_number: "",
+    country: "",
+    tel: "",
+  });
+
+  const handleChange = (e) =>
+    setFormState((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      if (
+        !email ||
+        !password ||
+        !salutation ||
+        !firstname ||
+        !lastname ||
+        !birth_date ||
+        !zip_code ||
+        !city ||
+        !street ||
+        !street_number ||
+        !country
+      )
+        throw new Error("Bitte alles ausfüllen.");
+      setLoadingAuthRequest(true);
+      const { data, error } = await registerUser({
+        email,
+        password,
+        salutation,
+        firstname,
+        lastname,
+        birth_date,
+        zip_code,
+        city,
+        street,
+        street_number,
+        country,
+      });
+      if (error) throw error;
+      setToken(data.token);
+      setIsAuthenticated(true);
+      setLoadingAuthRequest(false);
+      localStorage.setItem("token", data.token);
+    } catch (error) {
+      setLoadingAuthRequest(false);
+      console.log(error);
+    }
+  };
+
+  if (loadingAuthRequest) return <Loading />;
+  if (isAuthenticated) return <Navigate to="/auth" />;
+
   return (
     <section className="sign-up">
       <div className="description">
@@ -17,12 +106,24 @@ const SignUp = () => {
         </p>
       </div>
 
-      <form className="data">
+      <form className="data" onSubmit={handleSubmit}>
         <div className="login-data">
           <h4>Login-Daten</h4>
-          <input type="text" placeholder="E-Mail Adresse" />
-          <input type="password" placeholder="Passwort" />
-          <input type="password" placeholder="Passwort wiederholen" />
+          <input
+            id="email"
+            type="email"
+            placeholder="E-Mail Adresse"
+            value={email}
+            onChange={handleChange}
+          />
+          <input
+            id="password"
+            type="password"
+            placeholder="Passwort"
+            value={password}
+            onChange={handleChange}
+          />
+          {/* <input type="password" placeholder="Passwort wiederholen" /> */}
         </div>
 
         <div className="personal-data">
@@ -30,31 +131,83 @@ const SignUp = () => {
             <h4>Rechnungsadresse</h4>
             <a href="#">Sie sind ein Firmenkunde?</a>
           </div>
-          <select>
-            <option defaultValue={"selected"}>- Bitte Anrede wählen -</option>
-            <option value="1">Frau</option>
-            <option value="2">Herr</option>
-            <option value="3">Mensch</option>
+          <select id="salutation" value={salutation} onChange={handleChange}>
+            <option value="">- Bitte Anrede wählen -</option>
+            <option value="Frau">Frau</option>
+            <option value="Herr">Herr</option>
+            <option value="Mensch">Mensch</option>
           </select>
-          <input type="text" placeholder="Vorname" />
-          <input type="text" placeholder="Nachname" />
+          <input
+            id="firstname"
+            type="text"
+            placeholder="Vorname"
+            value={firstname}
+            onChange={handleChange}
+          />
+          <input
+            id="lastname"
+            type="text"
+            placeholder="Nachname"
+            value={lastname}
+            onChange={handleChange}
+          />
           <div className="street">
-            <input type="text" placeholder="Straße" className="item2" />
-            <input type="text" placeholder="Nr./ID" className="item1" />
+            <input
+              id="street"
+              type="text"
+              placeholder="Straße"
+              className="item2"
+              value={street}
+              onChange={handleChange}
+            />
+            <input
+              id="street_number"
+              type="text"
+              placeholder="Nr./ID"
+              className="item1"
+              value={street_number}
+              onChange={handleChange}
+            />
           </div>
           <input type="text" placeholder="Adresszusatz/ Postnummer" />
           <div className="town">
-            <input type="text" placeholder="PLZ" className="item1" />
-            <input type="text" placeholder="Stadt" className="item2" />
+            <input
+              id="zip_code"
+              type="text"
+              placeholder="PLZ"
+              className="item1"
+              value={zip_code}
+              onChange={handleChange}
+            />
+            <input
+              id="city"
+              type="text"
+              placeholder="Stadt"
+              value={city}
+              onChange={handleChange}
+              className="item2"
+            />
           </div>
-          <select>
-            <option defaultValue={"selected"}>- Bitte Land wählen -</option>
-            <option value="1">Deutschland</option>
-            <option value="2">Österreich</option>
-            <option value="3">Schweiz</option>
+          <select id="country" value={country} onChange={handleChange}>
+            <option value="">- Bitte Land wählen -</option>
+            <option value="Deutschland">Deutschland</option>
+            <option value="Österreich">Österreich</option>
+            <option value="Schweiz">Schweiz</option>
           </select>
-          <input type="text" placeholder="+49 39 53604246" />
-          <input type="text" placeholder="DD.MM.YYYY" />
+          <input
+            id="tel"
+            type="text"
+            placeholder="+49 39 53604246"
+            value={tel}
+            onChange={handleChange}
+          />
+          <input
+            id="birth_date"
+            type="text"
+            placeholder="DD.MM.YYYY"
+            value={birth_date}
+            onChange={handleChange}
+          />
         </div>
         <div className="delivery-address">
           <h4>Lieferadresse</h4>
@@ -67,7 +220,9 @@ const SignUp = () => {
             Abweichende Lieferadresse
           </label>
         </div>
-        <input type="submit" value="Kundenkonto anlegen" className="button" />
+        <button type="submit" className="button">
+          Kundenkonto anlegen
+        </button>
       </form>
     </section>
   );
