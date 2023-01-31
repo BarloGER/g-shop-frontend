69310a86de57f8d0cd2bb7040c988dbce1656552
BarloGER <barlozewski.mike@gmail.com>
2023-01-31 15:54:57 +0100

    Added NotFound.jsx for unkown routes

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Wohin, genau.</h1>
+      <img
+        src="https://www.derma-am.com/wp-content/uploads/2022/04/404.gif"
+        alt="Seite existiert nicht"
+      />
+    </div>
+  );
+};
+
+export default NotFound;
