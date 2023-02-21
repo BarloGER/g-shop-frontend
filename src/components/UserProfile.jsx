7308f7a552f8de2f8c763f6737bc80a36b274afc
BarloGER <barlozewski.mike@gmail.com>
2023-02-21 18:55:46 +0100

    Added test deleteAccount and logOut buttons in UserProfile.jsx

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,13 +1,26 @@
 import Loading from "./Loading";
+import { deleteUser } from "../utils/auth";
 
 // This component is used to display the user's profile if the user is logged in
 
-const UserProfile = ({ user }) => {
+const UserProfile = ({ user, loadingAuthRequest, isAuthenticated }) => {
+  const token = localStorage.getItem("token");
+  const deleteAccount = () => {
+    deleteUser(token);
+    localStorage.removeItem("token");
+  };
+
+  const logOut = () => {
+    localStorage.removeItem("token");
+  };
+
   return user ? (
     <div>
       <h1>
         Willkommen zurück, {user.firstname} {user.lastname}
       </h1>
+      <button onClick={logOut}>Logout</button>
+      <button onClick={deleteAccount}>Account löschen</button>
     </div>
   ) : (
     <Loading />
